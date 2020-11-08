4882ade82af27ce5d1506937d87f83fa9137335c
RogueNPC <jordanbtansay@gmail.com>
2020-11-08 12:25:13 -0800

    added get_balance method

--- BankAccount.py
+++ BankAccount.py
@@ -19,6 +19,10 @@ class BankAccount:
             print("Insufficient funds, you have been charged an overdraft fee of $10.")
             self.balance -= 10.00
 
+    def get_balance(self):
+        print(f"Hello {self.full_name}, you currently have a balance of ${self.balance} in your bank account.")
+        return self.balance
+
 first = BankAccount("Jim Lars")
 # print(first.__dict__)
 first.withdraw(20.49)
@@ -27,3 +31,4 @@ first.deposit(230.99)
 # print(first.balance)
 first.withdraw(20.49)
 # print(first.balance)
+first.get_balance()
