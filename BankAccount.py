3bd2610d56a9e03b893ef0f20a133973a6d96c62
RogueNPC <jordanbtansay@gmail.com>
2020-11-09 18:10:52 -0800

    fixed account_number generator

--- BankAccount.py
+++ BankAccount.py
@@ -1,11 +1,11 @@
 from random import randint
 
 class BankAccount:
-    account_number = randint(10000000, 99999999)
     routing_number = 123456789
     balance = 0
 
     def __init__(self, full_name):
+        self.account_number = randint(10000000, 99999999)
         self.full_name = full_name
 
     def deposit(self, amount):
